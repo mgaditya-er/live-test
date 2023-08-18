aaf465c827b7c146dd38a210e37f9968b886fbca
Yuvraj-Lolage <86282979+Yuvraj-Lolage@users.noreply.github.com>
2023-08-18 23:14:15 +0530

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',         # Database name (MYSQLDATABASE)
+        'USER': 'root',            # Database user (MYSQLUSER)
+        'PASSWORD': 'wu0MfUkkevf3Xlbbuiuc',  # Database password (MYSQLPASSWORD)
+        'HOST': 'containers-us-west-143.railway.app',  # Database host (MYSQLHOST)
+        'PORT': '5500',            # Database port (MYSQLPORT)
     }
 }
 
